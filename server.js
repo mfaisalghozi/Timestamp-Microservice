712c8f93890469e2e2ea9666de7dd68c0fda52d3
mfaisalghozi <mrafgaming@gmail.com>
2021-01-31 15:56:26 +0700

    Project Done

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({
+  optionsSuccessStatus: 200
+})); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,12 +23,43 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({
+    greeting: 'hello API'
+  });
+});
+
+//timestamp API endpoint ...
+app.get("/api/timestamp/", (req,res) => {
+  res.json({
+    unix: Date.now(),
+    utc: Date()
+  })
+})
+
+app.get("/api/timestamp/:dateOrUnix", (req, res) => {
+  let dateString = req.params.dateOrUnix;
+
+  if(/\d{5,}/.test(dateString)){
+    const dateInt = parseInt(dateString);
+    res.json({
+      unix: dateInt,
+      utc: new Date(dateInt).toUTCString()
+    })
+  }else{
+    let dateObject = new Date(dateString)
+    if(dateObject.toString() === "Invalid Date"){
+      res.json({error: "Invalid Date"});
+    }else{
+      res.json({
+        unix: dateObject.valueOf(),
+        utc: dateObject.toUTCString()
+      })
+    }
+  }
 });
 
 
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
